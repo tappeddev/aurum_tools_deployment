a58e3006635e809db8ce71e113c05f8f1440d4d3
stefanschaller <stefanschaller@users.noreply.github.com>
2026-02-17 08:38:13 +0000

    deploy: tappeddev/aurum_tools@453193bdca815592b956375db5e13f1be6756801

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c34c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba3d","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3254871937"
+    serviceWorkerVersion: "2926803986"
   }
 });
