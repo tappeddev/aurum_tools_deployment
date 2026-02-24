61a95a65b51a8454062d172a662b739538ee1f2d
TarekTolba1 <TarekTolba1@users.noreply.github.com>
2026-02-24 09:13:06 +0000

    deploy: tappeddev/aurum_tools@42ed97a1677e93c510c10c96da128ab626c6c76b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -97,9 +97,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/skwasm.wasm": "7e5f3afdd3b0747a1fd4517cea239898",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
-"flutter_bootstrap.js": "274c8eece475b0d5b5ba16d1522c2be9",
+"flutter_bootstrap.js": "6ac8b26c347c9c9daee330eb48c5cf08",
 "version.json": "536a0c25fecee4034a986a89a439fa1d",
-"main.dart.js": "589815feb4f793d402bf3313555d043d"};
+"main.dart.js": "c59c7ea51530622844aa49bf0e964776"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
